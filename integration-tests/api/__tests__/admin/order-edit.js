1f2c96368668be2bef97b27c9f3c2f2e13c08524
adrien2p <adrien.deperetti@gmail.com>
2022-09-12 12:07:42 +0200

    test(integration): Add premises of integration tests

---
+++ integration-tests/api/__tests__/admin/order-edit.js
@@ -0,0 +1,63 @@
+const path = require("path")
+
+const startServerWithEnvironment =
+  require("../../../helpers/start-server-with-environment").default
+const { useApi } = require("../../../helpers/use-api")
+const { useDb } = require("../../../helpers/use-db")
+
+const adminSeeder = require("../../helpers/admin-seeder")
+
+const adminHeaders = {
+  headers: {
+    Authorization: "Bearer test_token",
+  },
+}
+
+jest.setTimeout(30000)
+
+describe("[MEDUSA_FF_ORDER_EDIT] /admin/order-edits", () => {
+  let medusaProcess
+  let dbConnection
+
+  beforeAll(async () => {
+    const cwd = path.resolve(path.join(__dirname, "..", "..", ".."))
+    const [process, connection] = await startServerWithEnvironment({
+      cwd,
+      env: { MEDUSA_FF_ORDER_EDIT: true },
+      verbose: false,
+    })
+    dbConnection = connection
+    medusaProcess = process
+  })
+
+  afterAll(async () => {
+    const db = useDb()
+    await db.shutdown()
+
+    medusaProcess.kill()
+  })
+
+  describe("GET /admin/order-edits/:id", () => {
+    let orderEditId
+
+    beforeEach(async () => {
+      await adminSeeder(dbConnection)
+    })
+
+    afterEach(async () => {
+      const db = useDb()
+      return await db.teardown()
+    })
+
+    it("gets order edit", async () => {
+      const api = useApi()
+
+      const response = await api.get(
+        `/admin/order-edits/${orderEditId}`,
+        adminHeaders
+      )
+
+      expect(response.status).toEqual(200)
+    })
+  })
+})
---
+++ integration-tests/api/__tests__/store/order-edit.js
@@ -0,0 +1,52 @@
+const path = require("path")
+
+const startServerWithEnvironment =
+  require("../../../helpers/start-server-with-environment").default
+const { useApi } = require("../../../helpers/use-api")
+const { useDb } = require("../../../helpers/use-db")
+
+jest.setTimeout(30000)
+
+describe("[MEDUSA_FF_ORDER_EDIT] /store/order-edits", () => {
+  let medusaProcess
+  let dbConnection
+
+  beforeAll(async () => {
+    const cwd = path.resolve(path.join(__dirname, "..", "..", ".."))
+    const [process, connection] = await startServerWithEnvironment({
+      cwd,
+      env: { MEDUSA_FF_ORDER_EDIT: true },
+      verbose: false,
+    })
+    dbConnection = connection
+    medusaProcess = process
+  })
+
+  afterAll(async () => {
+    const db = useDb()
+    await db.shutdown()
+
+    medusaProcess.kill()
+  })
+
+  describe("GET /store/order-edits/:id", () => {
+    let orderEditId
+
+    beforeEach(async () => {
+      await adminSeeder(dbConnection)
+    })
+
+    afterEach(async () => {
+      const db = useDb()
+      return await db.teardown()
+    })
+
+    it("gets order edit", async () => {
+      const api = useApi()
+
+      const response = await api.get(`/store/order-edits/${orderEditId}`)
+
+      expect(response.status).toEqual(200)
+    })
+  })
+})
